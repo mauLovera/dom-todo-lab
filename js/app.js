0d4496fa586ce788ff33a5aa7ded8627ff6d4d87
mauLovera <mauricioloveraedu@gmail.com>
2022-05-31 16:46:17 -0400

    Working build

--- js/app.js
+++ js/app.js
@@ -4,5 +4,17 @@ const h1 = document.querySelector(`h1`)
 const input = document.querySelector(`#input`)
 const button = document.querySelector(`#submit-button`)
 const ulList = document.querySelector(`#todo-list`)
+const form = document.querySelector(`#form`)
 
 
+button.addEventListener(`click`, addLi)  
+
+
+
+function addLi(evt) {
+  let newLi = document.createElement(`li`)
+  newLi.textContent = input.value
+  input.value = ``
+  ulList.appendChild(newLi)
+  console.log(`DO YOU WORK?`)
+}
