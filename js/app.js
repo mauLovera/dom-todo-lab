d25a1895eaec989f59607e697e0b6ac187d624d0
mauLovera <mauricioloveraedu@gmail.com>
2022-05-31 16:27:31 -0400

    create CERs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log(`sanity`)
+// Cashed Element References //
+
+const h1 = document.querySelector(`h1`)
+const input = document.querySelector(`#input`)
+const button = document.querySelector(`#submit-button`)
+const ulList = document.querySelector(`#todo-list`)
+
+
