67b2e7ef7472857eb94e8d3030e2c760585e3b3d
mauLovera <mauricioloveraedu@gmail.com>
2022-06-01 15:32:05 -0400

    add keystroke li deletion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Cashed Element References //
 
 const h1 = document.querySelector(`h1`)
+const form = document.querySelector(`#main`)
 const input = document.querySelector(`#input`)
 
 const button = document.querySelector(`#submit-button`)
@@ -8,20 +9,20 @@ const resetButton = document.querySelector(`#reset-button`)
 
 const ulList = document.querySelector(`#todo-list`)
 
-// Remove on Click //
-const remove = document.querySelector(`#todo-list`)
+// Remove on Click and Del Key //
+const clickRemove = document.querySelector(`#todo-list`)
+const delRemove = document.querySelector(`#todo-list`)
 
 // Variables //
 
 let newLi = document.createElement(`li`)
 
 // Event Listener //
-
 button.addEventListener(`click`, addLi)
-remove.addEventListener(`click`, removeLi) 
+clickRemove.addEventListener(`click`, removeLi) 
 resetButton.addEventListener(`click`, resetUl) 
-
-
+form.addEventListener(`submit`, formEvt)
+window.addEventListener(`keydown`, delLi)
 
 // Functions //
 
@@ -33,14 +34,14 @@ function addLi() {
   input.value = ``
   ulList.appendChild(newLi)
   newLi.className = 'list-item'
-  console.log(`DO YOU WORK?`)
+  console.log(`li added`)
   }
 }
 
 // On click of the li items within Ul it will remove them
 function removeLi() {
   ulList.removeChild(ulList.querySelector(`.list-item`))
-  console.log(`Do you work?`)
+  console.log(`li was clicked`)
 }
 
 // On click of reset button it will remove every child within the ul with a while loop
@@ -48,5 +49,26 @@ function resetUl() {
   let newUl = document.querySelector(`#todo-list`)
   while (newUl.firstChild) { // while newUl.firstChild is TRUE - it EXISTS - remove each child of it
     newUl.removeChild(newUl.lastChild)
+    console.log(`ul reset`)
+  }
+}
+
+// stops form from refreshing page
+function formEvt(evt) {
+  evt.preventDefault();
+}
+
+// function delLi(evt) {
+//   if (evt.keyCode === 8) {
+//     ulList.removeChild(ulList.querySelector(`.list-item`))
+//     console.log('Keys were pressed')
+//   }
+// }
+
+function delLi(evt) {
+  switch (evt.keyCode) {
+    case 8 && 16:
+      ulList.removeChild(ulList.querySelector(`.list-item`))
+      console.log(`keys were pressed`)
   }
 }
