106affcf97bb6a7f1f439764d2018fed2765b030
mauLovera <mauricioloveraedu@gmail.com>
2022-06-02 11:23:23 -0400

    add comments and variable names

--- js/app.js
+++ js/app.js
@@ -1,61 +1,60 @@
+/* --------------------------Variables--------------------------*/ 
 
-// onkeydown = function holdKey(evt) {
-//   keyHolder[evt.keyCode] = evt.type === `keydown` 
-//   if (keyHolder[8] && keyHolder[16]) {
-  //     ulList.removeChild(ulList.querySelector(`.list-item`))
-//     keyHolder.splice(0, keyHolder.length)
-//   } else if (!keyHolder[16]) {
-//     keyHolder.splice(0, keyHolder.length)
-//     console.log(`works`)
-//   } 
-//   console.log(keyHolder)
-// }
-
-// Variables //
-  
-let keyHolder = []
+/* KeyCode Data Arrays */
+let downKeyHolder = []
 let upKeyHolder = []
-let newLi = document.createElement(`li`)
 
-// Cashed Element References //
+/* Create Elements */
+let newLi = document.createElement('li')
 
-const h1 = document.querySelector(`h1`)
-const form = document.querySelector(`#main`)
-const input = document.querySelector(`#input`)
+/* -----------------Cashed Element References-------------------*/ 
 
-const button = document.querySelector(`#submit-button`)
-const resetButton = document.querySelector(`#reset-button`)
+const h1 = document.querySelector('h1')
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
 
-const ulList = document.querySelector(`#todo-list`)
+const postButton = document.querySelector('#submit-button')
+const resetButton = document.querySelector('#reset-button')
 
-// Remove on Click and Del Key //
-const clickRemove = document.querySelector(`#todo-list`)
-const delRemove = document.querySelector(`#todo-list`)
+const ulList = document.querySelector('#todo-list')
 
+const clickRemove = document.querySelector('#todo-list')
+const delRemove = document.querySelector('#todo-list')
 
-// Event Listener //
-button.addEventListener(`click`, addLi)
-clickRemove.addEventListener(`click`, removeLi) 
-resetButton.addEventListener(`click`, resetUl) 
-form.addEventListener(`submit`, formEvt)
-window.addEventListener(`keydown`, keyHolder)
-window.addEventListener(`keyup`, upKeyHolder)
+/* -----------------------Event Listeners------------------------*/ 
 
-// Functions //
+// On click: add the input value as an li to the ul
+postButton.addEventListener('click', addLi)
+form.addEventListener('submit', formEvt) // on 'enter'
 
-// On click of the Add todo button it will all the value of the input field to the todo ul as an li 
+// On click: remove the li from the ul
+clickRemove.addEventListener('click', removeLi) 
+
+// On click: remove all li children from ul 
+resetButton.addEventListener('click', resetUl)
+
+// KeyCode Listeners and Keyboard Commands
+window.addEventListener('keydown', downKeyHolder)
+window.addEventListener('keyup', upKeyHolder)
+
+/* --------------------------Functions---------------------------*/ 
+
 function addLi() {
   if (input.value !== '') {
-    newLi = document.createElement(`li`)
+    newLi = document.createElement('li')
     newLi.textContent = input.value
-    input.value = ``
+    input.value = ''
     ulList.appendChild(newLi)
     newLi.className = 'list-item'
-    console.log(`li added`)
+    console.log('li added')
   }
 }
 
-// On click of the li items within Ul it will remove them
+/** 
+  * Removes a created li from the ul
+  * On click of the li 
+  */  
+
 function removeLi() {
   ulList.removeChild(ulList.querySelector(`.list-item`))
   console.log(`li was clicked`)
@@ -82,36 +81,39 @@ function formEvt(evt) {
 
 // DEFINITELY REFRACTOR ANOTHER DAY
 
+// bug: user can submit just empty string with a space in it 
+
+
 onkeydown = function holdKey(evt) {
-  keyHolder[evt.keyCode] = evt.type === `keydown` 
-  if (keyHolder[8] && keyHolder[16] && keyHolder[13]) { 
+  downKeyHolder[evt.keyCode] = evt.type === `keydown` 
+  if (downKeyHolder[8] && downKeyHolder[16] && downKeyHolder[13]) { 
     // if delete shift and enter exist at the same time in the array - empty the array 
-      keyHolder.splice(0, keyHolder.length)
+      downKeyHolder.splice(0, downKeyHolder.length)
       console.log(`empty with shift delete and enter`)
-  } else if (keyHolder[8] && keyHolder[16] && keyHolder[32]) { // shift delete enter reset 
+  } else if (downKeyHolder[8] && downKeyHolder[16] && downKeyHolder[32]) { // shift delete enter reset 
       console.log('RESET')
       resetUl()
-  } else if (keyHolder[16] && keyHolder[8]) { 
+  } else if (downKeyHolder[16] && downKeyHolder[8]) { 
     // if shift and delete are pressed - delete a li and empty the array
       ulList.removeChild(ulList.querySelector(`.list-item`))
       do {
-        keyHolder.splice(0, keyHolder.length, 16)
+        downKeyHolder.splice(0, downKeyHolder.length, 16)
         console.log(`do while`)
-      } while (keyHolder === [])
+      } while (downKeyHolder === [])
       console.log(`empty with shift and delete`)
-  } else if (keyHolder[8] && !keyHolder[16]) { 
+  } else if (downKeyHolder[8] && !downKeyHolder[16]) { 
     // if delete exists (because the user deleted a word in input) and shift does not - empty the array
-      keyHolder.splice(0, keyHolder.length)
+      downKeyHolder.splice(0, downKeyHolder.length)
       console.log(`empty with delete and no shift`)
-  } else if (!keyHolder[16] && !keyHolder[8] && !keyHolder[32]) {
+  } else if (!downKeyHolder[16] && !downKeyHolder[8] && !downKeyHolder[32]) {
     // if shift and delete does not exist - dont log the array 
-      keyHolder.splice(0, keyHolder.length)
+      downKeyHolder.splice(0, downKeyHolder.length)
       console.log(`empty because its not shift or delete`)
-  } else if (keyHolder[13]) { // if enter exists - empty the array 
-      keyHolder.splice(0, keyHolder.length)
+  } else if (downKeyHolder[13]) { // if enter exists - empty the array 
+      downKeyHolder.splice(0, downKeyHolder.length)
       console.log(`empty with enter`)
   } 
-  console.log(keyHolder)
+  console.log(downKeyHolder)
 }
 
 // adds a safeguard for clearing array if shift is lifted which in turn fixes bug that deleted li when 
@@ -120,7 +122,7 @@ onkeydown = function holdKey(evt) {
 onkeyup = function upKey(evt) {
   upKeyHolder[evt.keyCode] = evt.type === `keyup`
   if (upKeyHolder[16]) {
-    keyHolder.splice(0, keyHolder.length)
+    downKeyHolder.splice(0, downKeyHolder.length)
     console.log(`log something please`)
   } // else if (upKeyHolder[8]) {
   // keyHolder.splice(0, 0, 16)
